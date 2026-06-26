1e88e8c23b1c88edf6b11fdea2a521842c00918e
taeyoung0524 <miseoseo1@naver.com>
2026-06-26 14:51:48 +0900

    태영이 경로(절대경로)로 수정

--- src/config.py
+++ src/config.py
@@ -1,8 +1,9 @@
 # 경로랑 모델명 같은 설정값 모아두는 파일
 import os
 
-DATA_DIR = "data"
+DATA_DIR = "/home/undergraduate/20231372_TY/VLM-ExplainEval/data"
 IMAGE_DIR = "/home/undergraduate/20231372_TY/VLM-ExplainEval/data/activitynet_image"
 TEST_JSON = os.path.join(DATA_DIR, "test.json")
+RESULT_DIR = "/home/undergraduate/20221373_YY/VLM-ExplainEval/results"
 MODEL_NAME = "gemini-2.5-flash-lite"
 NUM_FRAMES = 8
