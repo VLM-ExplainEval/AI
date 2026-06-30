4bd1e46a8ff3b73db466c7fd844679467ee6dd8b
yeyeong5016 <20221373@sungshin.ac.kr>
2026-06-30 15:58:12 +0900

    이미지 경로 추가

--- src/config.py
+++ src/config.py
@@ -4,5 +4,7 @@ import os
 DATA_DIR = "/home/undergraduate/20231372_TY/VLM-ExplainEval/data"
 IMAGE_DIR = "/home/undergraduate/20231372_TY/VLM-ExplainEval/data/activitynet_image"
 TEST_JSON = os.path.join(DATA_DIR, "test.json")
+TRAIN_JSON = os.path.join(DATA_DIR, "train.json")
+TRAIN_IMAGE_DIR = "/home/undergraduate/20231372_TY/VLM-ExplainEval/data/8frames_train"
 RESULT_DIR = "/home/undergraduate/20221373_YY/VLM-ExplainEval/results"
 MODEL_NAME = "gemini-2.5-flash"
